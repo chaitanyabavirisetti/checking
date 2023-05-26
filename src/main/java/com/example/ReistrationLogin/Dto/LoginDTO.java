246524c218f6b36d834e2679f08ccab1fa570063
Chaitanya <chaitanyabavirisetti9@gmail.com>
2023-05-26 12:26:42 +0530

    2nd commit

---
+++ src/main/java/com/example/ReistrationLogin/Dto/LoginDTO.java
@@ -0,0 +1,21 @@
+package com.example.ReistrationLogin.Dto;
+
+import lombok.*;
+
+@Builder
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+
+
+
+
+public class LoginDTO
+{
+        private String email;
+        private String password;
+
+
+}
